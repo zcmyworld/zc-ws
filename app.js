1c14dcd6d6f4020ddaa92361a02642b6cb44ee9b
zcmyworld <zhicong@zcmyworld.com>
2015-02-03 10:54:01 +0800

    big text send

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http');
 
-var user = {}
+var user = {};
+var userCallback = {};
 var userNum = 0;
 
 var myServer = http.createServer(function(request, response) {
@@ -51,9 +52,10 @@ var http = require('http');
 var server = http.createServer(function(e) {});
 var crypto = require('crypto');
 server.on('upgrade', function(req, socket, body) {
-	// req.connection.on('close', function() {
-	// 	console.log('connection close')
-	// })
+	req.connection.on('close', function() {
+		console.log('connection close')
+	})
+
 	// socket.on('close', function() {
 	// 	console.log('socket close')
 	// })
@@ -64,62 +66,124 @@ server.on('upgrade', function(req, socket, body) {
 	shasum.update(key + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11");
 	key = shasum.digest('base64');
 	var headers = [
-		'HTTP/1.1 101 Switching Protocols', 'Upgrade: websocket', 'Connection: Upgrade', 'Sec-WebSocket-Accept: ' + key
+		'HTTP/1.1 101 Switching Protocols'
+		, 'Upgrade: websocket'
+		, 'Connection: Upgrade'
+		, 'Sec-WebSocket-Accept: ' + key
 	];
 	socket.write(headers.concat('', '').join('\r\n'));
 
-	// console.log(req.headers.cookie)
-	
+	// cookieStr = req.headers.cookie;
+	// var cookieArr = cookieStr.split(';')
+	// var cookieJson = {}
+	// for (var i in cookieArr) {
+	// 	cookieArr[i] = cookieArr[i].split('=')
+	// 	cookieJson[cookieArr[i][0]] = cookieArr[i][1]
+	// }
+	// query(cookieJson.user, function() {
+	// 	var data = "中文";
+	// 	data = new Buffer(data);
+	// 	var dataLength = data.length;
+	// 	var dataOffset = 2; //第一字节存FIN,RSV1-3和opcode 第二字节存mask和数据长度
+	// 	var payloadLen = dataLength; //Payload len
+	// 	var totalLength = dataLength + dataOffset; //完整长度是数据长度+前两字节
+	// 	var outputBuffer = new Buffer(totalLength);
+	// 	outputBuffer[0] = 0x81; //代表FIN,RSV1-3和opcode
+	// 	outputBuffer[1] = payloadLen; //代表mask和数据长度
+	// 	data.copy(outputBuffer, dataOffset); //用data从outputBuffer的第2位开始覆盖其数据
+	// 	socket.write(outputBuffer, 'binary'); //将数据以二进制格式传到客户端
+	// })
+
+	// setTimeout(function() {
+	// 	//发送消息给user1
+	// 	console.log('执行callback')
+	// 	// userCallback['user1']();
+	// }, 5000);
 	//推送消息
-	var data = "中文";
+	// var data = "中文";
+	var data = "";
+	for (var i = 0; i < 85535; i++) {
+		data += 1;
+	};
 	data = new Buffer(data);
 	var dataLength = data.length;
 	var dataOffset = 2; //第一字节存FIN,RSV1-3和opcode 第二字节存mask和数据长度
-	var payloadLen = dataLength; //Payload len
-	var totalLength = dataLength + dataOffset; //完整长度是数据长度+前两字节
+	var payloadLen = dataLength;
+
+	if (dataLength >= 65536) {
+		dataOffset += 8;
+		payloadLen = 127;
+	} else if (dataLength > 125) {
+		dataOffset += 2; //2字节Extended payload length
+		payloadLen = 126;
+	}
+	var totalLength = dataLength + dataOffset;
+	if (dataLength >= 65536) {
+		totalLength = dataOffset
+	}
 	var outputBuffer = new Buffer(totalLength);
-	outputBuffer[0] = 0x81; //代表FIN,RSV1-3和opcode
+	outputBuffer[0] = 0x80 | 0x1;
 	outputBuffer[1] = payloadLen; //代表mask和数据长度
-	data.copy(outputBuffer, dataOffset); //用data从outputBuffer的第2位开始覆盖其数据
-	socket.write(outputBuffer, 'binary'); //将数据以二进制格式传到客户端
+
+	if (payloadLen == 126) {
+		outputBuffer[2] = (dataLength & 0xff00) >> 8
+		outputBuffer[3] = dataLength & 0xff
+	}
+	if (payloadLen == 127) {
+		outputBuffer[2] = (dataLength & 0xff00000000000000) >> 512
+		outputBuffer[3] = (dataLength & 0xff000000000000) >> 256
+		outputBuffer[4] = (dataLength & 0xff0000000000) >> 128
+		outputBuffer[5] = (dataLength & 0xff00000000) >> 64
+		outputBuffer[6] = (dataLength & 0xff000000) >> 32
+		outputBuffer[7] = (dataLength & 0xff0000) >> 16
+		outputBuffer[8] = (dataLength & 0xff00) >> 8
+		outputBuffer[9] = dataLength & 0xff
+	}
+
+	if (dataLength >= 65536) {
+		socket.write(outputBuffer, 'binary');
+		socket.write(data, 'binary')
+	} else {
+		data.copy(outputBuffer, dataOffset); //用data从outputBuffer的第2位开始覆盖其数据
+		socket.write(outputBuffer, 'binary'); //将数据以二进制格式传到客户端	
+	}
 	// socketList[socket] = 1;
 	//接收消息
-	// socket.on('data', function(data) {
-	// 	if (data[0].toString(2).slice(4) == 1000) {
-	// 		console.log('连接断开');
-	// 		return;
-	// 	}
-	// 	console.log(data[0].toString(2).slice(4))
-	// 	var mask_key = data.slice(2, 6) //获取mask-key
-	// 	var Payload_data = data.slice(6) //获取payload data
-	// 	for (var i = 0; i < Payload_data.length; i++) {
-	// 		Payload_data[i] = Payload_data[i] ^ mask_key[i % 4];
-	// 	} //^异或运算符
-	// 	console.log(Payload_data.toString())
-	// })
-	setInterval(function() {
-		console.log('pong')
-		var data = "";
-		data = new Buffer(data);
-		var dataLength = data.length;
-		var dataOffset = 2; //第一字节存FIN,RSV1-3和opcode 第二字节存mask和数据长度
-		var payloadLen = dataLength; //Payload len
-		var totalLength = dataLength + dataOffset; //完整长度是数据长度+前两字节
-		var outputBuffer = new Buffer(totalLength);
-		// outputBuffer[0] = 0x81; //代表FIN,RSV1-3和opcode
-		outputBuffer[0] = 0x80 | 0xA;
-		outputBuffer[1] = payloadLen; //代表mask和数据长度
-		data.copy(outputBuffer, dataOffset); //用data从outputBuffer的第2位开始覆盖其数据
-		socket.write(outputBuffer, 'binary'); //将数据以二进制格式传到客户端
-	}, 3000)
+	socket.on('data', function(data) {
+		if (data[0].toString(2).slice(4) == 1000) {
+			console.log('连接断开');
+			return;
+		}
+		var mask_key = data.slice(2, 6) //获取mask-key
+		var Payload_data = data.slice(6) //获取payload data
+		for (var i = 0; i < Payload_data.length; i++) {
+			Payload_data[i] = Payload_data[i] ^ mask_key[i % 4];
+		} //^异或运算符
+		// console.log(Payload_data.toString())
+	})
+
+	// setInterval(function() {
+	// 	console.log('pong')
+	// 	var data = "pong";
+	// 	data = new Buffer(data);
+	// 	var dataLength = data.length;
+	// 	var dataOffset = 2; //第一字节存FIN,RSV1-3和opcode 第二字节存mask和数据长度
+	// 	var payloadLen = dataLength; //Payload len
+	// 	var totalLength = dataLength + dataOffset; //完整长度是数据长度+前两字节
+	// 	var outputBuffer = new Buffer(totalLength);
+	// 	// outputBuffer[0] = 0x81; //代表FIN,RSV1-3和opcode
+	// 	outputBuffer[0] = 0x80 | 0x1;
+	// 	outputBuffer[1] = payloadLen; //代表mask和数据长度
+	// 	data.copy(outputBuffer, dataOffset); //用data从outputBuffer的第2位开始覆盖其数据
+	// 	socket.write(outputBuffer, 'binary'); //将数据以二进制格式传到客户端
+	// }, 3000)
 });
-server.on('connection', function() {
-	console.log('有链接')
-})
 
 server.listen(4180);
 // server.timeout = 5000
 
-setInterval(function() {
-	console.log(user)
-}, 5000)
+
+function query(userName, callback) {
+	userCallback[userName] = callback;
+}
+
--- cli.js
+++ cli.js
@@ -1,28 +1,20 @@
-var net = require('net'),
-	websocket = require('websocket-driver');
+var http = require('http')
+var options = {
+	hostname: '127.0.0.1',
+	port: 4180,
+	method:'GET',
+	headers: {
+		"Sec-WebSocket-Key": "9ytoObvofmhVipGBSOGHpw==",
+		"Sec-WebSocket-Version": 13,
+		"Upgrade": "websocket"
+	}
+};
 
-var driver = websocket.client('ws://127.0.0.1/socket'),
-	tcp = net.connect(4180, '127.0.0.1');//监听4180端口
-
-tcp.pipe(driver.io).pipe(tcp);
-
-tcp.on('connect', function() {
-	driver.start();
+var req = http.request(options, function(res) {
+	console.log('nima')
+	res.setEncoding('utf8');
+	res.on('data', function(chunk) {
+		console.log('BODY: ' + chunk);
+	});
 });
-
-driver.messages.on('data', function(message) {
-	console.log('Got a message', message);//接收来自浏览器的消息
-});
-
-// function Person(name, age) {
-// 	this.name = name;
-// 	this.age = age;
-// 	// console.log(name + "" + age)
-// }
-// function Student(name,age){
-// 	console.log(Person.apply(this,['name','age']));
-// 	console.log(this.name)
-// 	console.log(this.age)
-// }
-// // Student()
-// console.log(Person.apply(null, [1, 1]))
+req.end();
